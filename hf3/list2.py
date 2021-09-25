232e6e0402459ee1c178fdd97daec918613098bf
petrusjt <jozsefpetrus99@gmail.com>
2021-09-25 11:02:37 +0200

    finished hf 3

---
+++ hf3/list2.py
@@ -0,0 +1,85 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+# A magyar változatot készítette:
+# Szathmáry László (jabba.laci@gmail.com)
+# frissítés Python 3-ra: 2016.09.09.
+
+
+# D.
+# Bemenet: számok rendezett listája.
+# Kimenet: a bementből távolítsuk el az ismétlődéseket, vagyis az
+# eredményben egy szám csak egyszer szerepeljen.
+# Példa: [1, 2, 2, 3] -> [1, 2, 3].
+# Készíthetünk egy új listát, vagy módosíthatjuk a bemeneti listát is.
+def remove_adjacent(nums):
+    unique_nums = []
+    for num in nums:
+        if num not in unique_nums:
+            unique_nums.append(num)
+    return unique_nums
+
+# E.
+# Bemenet: két lista, mindkettőben az elemek növekvő sorrendbe rendezve.
+# Kimenet: egy összefésült lista, melyben az elemek rendezve szerepelnek.
+def list_merge(list1, list2):
+    i = 0
+    j = 0
+
+    merged_list = []
+    while i < len(list1) and j < len(list2):
+        if list1[i] < list2[j]:
+            merged_list.append(list1[i])
+            i += 1
+        else:
+            merged_list.append(list1[j])
+            j += 1
+
+    if i >= len(list1):
+        for k in range(j, len(list2)):
+            merged_list.append(list2[k])
+    else:
+        for l in range(i, len(list1)):
+            merged_list.append(list1[l])
+    return merged_list
+
+
+# Egy egyszerű teszt fv. Kiírja az egyes fv.-ek visszaadott értékét, ill.
+# azt is, hogy mit kellett volna visszaadniuk.
+def test(got, expected):
+    if got == expected:
+        prefix = ' OK '
+    else:
+        prefix = '  X '
+    print('{p} got: {g}; expected: {e}'.format(p=prefix, g=got, e=expected))
+
+
+# Ezt ne módosítsuk.
+# A main() fv. meghívja a fenti fv.-eket különféle paraméterekkel,
+# s a test() fv. segítségével megnézi, hogy az eredmények helyesek-e.
+def main():
+    print('remove_adjacent')
+    test(remove_adjacent([1, 2, 2, 3]), [1, 2, 3])
+    test(remove_adjacent([2, 2, 3, 3, 3]), [2, 3])
+    test(remove_adjacent([]), [])
+
+    print()
+    print('list_merge')
+    test(list_merge(['aa', 'xx', 'zz'], ['bb', 'cc']),
+         ['aa', 'bb', 'cc', 'xx', 'zz'])
+    test(list_merge(['aa', 'xx'], ['bb', 'cc', 'zz']),
+         ['aa', 'bb', 'cc', 'xx', 'zz'])
+    test(list_merge(['aa', 'aa'], ['aa', 'bb', 'bb']),
+         ['aa', 'aa', 'aa', 'bb', 'bb'])
+
+#############################################################################
+
+if __name__ == '__main__':
+    main()
---
+++ hf3/sztring_tisztitas.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def sztring_tisztit(string : str):
+    tiszta_string = ""
+    for character in string:
+        if not character.isspace():
+            tiszta_string += character
+
+    return tiszta_string
+
+
+def main():
+    sztringek = ["192.20.246.138:\n 6666", "206.130.99.82:\n8080"]
+    for sztring in sztringek:
+        print(sztring_tisztit(sztring))
+
+
+if __name__ == "__main__":
+    main()

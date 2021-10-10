03301035e337d2bc325fcb07387dbc264d9cb63e
petrusjt <jozsefpetrus99@gmail.com>
2021-10-10 19:22:31 +0200

    asd

--- hf5/2021.py
+++ hf5/2021.py
@@ -6,7 +6,10 @@ TWO = str(ord('c') - ord('a'))
 
 
 def main():
-    """Prints two thousand twenty one as a number without using numbers in the source code."""
+    """Prints two thousand twenty one.
+    
+    Print two thousand twenty one as a number without using numbers in
+    the source code."""
     
     print(TWO + ZERO + TWO + ONE)
 

409b5508559dce74d9ffdf2a9022fb68e7776d00
petrusjt <jozsefpetrus99@gmail.com>
2021-10-10 18:09:13 +0200

    helpful commit message

--- hf5/bizonyos_karakterek.py
+++ hf5/bizonyos_karakterek.py
@@ -10,9 +10,9 @@ def valid(text, chars="ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"):
 def main():
     """Calls valid with different parameters and prints result."""
 
-    print("Barking!", valid("Barking!"), sep=" -> ")
-    print("KL754", valid("KL754", chars="0123456789"), sep=" -> ")
-    print("BEAN", valid("BEAN", chars="abcdefghijklmnopqrstuvwxyz"), sep=" -> ")
+    print("Barking!", valid("Barking!"), sep="->")
+    print("KL754", valid("KL754", chars="0123456789"), sep="->")
+    print("BEAN", valid("BEAN", chars="abcdefghijklmnopqrstuvwxyz"), sep="->")
 
 
 if __name__ == "__main__":

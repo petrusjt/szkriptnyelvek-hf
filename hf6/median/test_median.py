5e97a3fdfc353e28a0cae9f6c0f13f821497e1ee
petrusjt <jozsefpetrus99@gmail.com>
2021-10-16 17:42:50 +0200

    fixed test_median.py

--- hf6/median/test_median.py
+++ hf6/median/test_median.py
@@ -1,7 +1,7 @@
 from median import get_median
 
 def test_median():
-    get_median([1, 2, 3, 4, 5]) == 3
-    get_median([3, 1, 2, 5, 3]) == 3
-    get_median([1, 300, 2, 200, 1]) == 2
-    get_median([3, 6, 20, 99, 10, 15]) == 12.5
+    assert get_median([1, 2, 3, 4, 5]) == 3
+    assert get_median([3, 1, 2, 5, 3]) == 3
+    assert get_median([1, 300, 2, 200, 1]) == 2
+    assert get_median([3, 6, 20, 99, 10, 15]) == 12.5

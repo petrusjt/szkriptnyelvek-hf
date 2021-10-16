bfb8d507a9d29278f8227fb870b5451535b56196
petrusjt <jozsefpetrus99@gmail.com>
2021-10-16 17:43:09 +0200

    fixed median.py

--- hf6/median/median.py
+++ hf6/median/median.py
@@ -3,11 +3,11 @@
 
 def get_median(li):
     lista = sorted(li)
-    if len(lista) % 2 == 0:
+    if len(lista) % 2 == 1:
         return lista[len(lista) // 2]
     else:
-        m_1 = lista[len(lista) // 2]
-        m_2 = lista[len(lista) // 2 + 1]
+        m_1 = lista[len(lista) // 2 - 1]
+        m_2 = lista[len(lista) // 2]
         return (m_1 + m_2) / 2
 
 

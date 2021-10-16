3609d8273905cfe0e7a0040d3f223bd1c054f393
petrusjt <jozsefpetrus99@gmail.com>
2021-10-16 17:45:58 +0200

    fixed line

--- hf6/szamjegyek_osszege/test_szamjegyek_osszege.py
+++ hf6/szamjegyek_osszege/test_szamjegyek_osszege.py
@@ -3,6 +3,6 @@ from szamjegyek_osszege import get_szamjegyek_osszege
 
 def test_get_szamjegyek_osszege():
     assert get_szamjegyek_osszege(2 ** 1000) == 1366
-    assert 10 == get_szamjegyek_osszege(19) == 10
+    assert get_szamjegyek_osszege(19) == 10
     assert get_szamjegyek_osszege(30) == 3
     assert get_szamjegyek_osszege(23532441414) == 33

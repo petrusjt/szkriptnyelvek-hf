ef514a2edfe23ebe423ad70154f0026639e31cb4
petrusjt <jozsefpetrus99@gmail.com>
2021-10-16 17:28:25 +0200

    converted asserts to 'assert 1 + 1 == 2' format

--- hf6/szamjegyek_osszege/test_szamjegyek_osszege.py
+++ hf6/szamjegyek_osszege/test_szamjegyek_osszege.py
@@ -2,7 +2,7 @@ from szamjegyek_osszege import get_szamjegyek_osszege
 
 
 def test_get_szamjegyek_osszege():
-    assert 1366 == get_szamjegyek_osszege(2 ** 1000)
-    assert 10 == get_szamjegyek_osszege(19)
-    assert 3 == get_szamjegyek_osszege(30)
-    assert 33 == get_szamjegyek_osszege(23532441414)
+    assert get_szamjegyek_osszege(2 ** 1000) == 1366
+    assert 10 == get_szamjegyek_osszege(19) == 10
+    assert get_szamjegyek_osszege(30) == 3
+    assert get_szamjegyek_osszege(23532441414) == 33
